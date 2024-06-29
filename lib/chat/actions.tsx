248eafdbb9475935ddb37f3255c31d9dfc6729aa
emanom <emumanom@gmail.com>
2024-06-30 01:34:03 +1000

    up

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -44,7 +44,7 @@ import rehypeStringify from 'rehype-stringify';
 import { promises as fs } from 'fs';
 import path from 'path';
 
-import styles from './syles/MarkdownContent.module.css'
+//import styles from './syles/MarkdownContent.module.css'
 
 
 async function markdownToHtml(markdown: string) {
