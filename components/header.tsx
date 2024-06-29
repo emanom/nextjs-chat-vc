02268fd63ca02bfe2a053cd24473f145dc1ae80e
emanom <emumanom@gmail.com>
2024-06-29 16:51:25 +1000

    Exercise

--- components/header.tsx
+++ components/header.tsx
@@ -58,7 +58,7 @@ export function Header() {
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          href="https://github.com/emanom/nextjs-chat-vc/tree/main"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
